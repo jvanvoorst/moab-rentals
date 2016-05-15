91e9f9ce4cde42e562f9b81e6567f13248fba63b
Justin <justin.vanvoorst@gmail.com>
2016-05-14 18:14:36 -0600

    added welcome view

---
+++ moabApp.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.sendFile('/html/index.html', {root: './public'});
+});
+
+var port = 8081;
+app.listen(port, function (){
+    console.log('Server running on port ' + port);
+});
--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,13 @@
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'html/welcome.html',
+        controller: 'mainController'
+    });
+}]);
+
+
 app.controller('mainController', ['$scope', function($scope) {
 
 }]);
