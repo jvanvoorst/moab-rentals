86b16eefd956f2f996947fc1c78ff72a6ab7696a
Justin <justin.vanvoorst@gmail.com>
2016-05-21 17:34:30 -0600

    added photo carousel

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,50 @@
-var app = angular.module('myApp', ['ngRoute']);
+var app = angular.module('myApp', ['ngRoute', 'ngAnimate', 'ui.bootstrap', 'ngTouch']);
 
 app.config(['$routeProvider', function($routeProvider) {
     $routeProvider
     .when('/', {
         templateUrl: 'html/welcome.html',
-        controller: 'mainController'
+        controller: 'mainCntrl'
     })
     .when('/short', {
         templateUrl: 'html/short.html',
-        controller: 'mainController'
+        controller: 'mainCntrl'
     })
+    .when('/short-photos', {
+        templateUrl: 'html/short-carousel.html',
+        controller: 'carouselCntrl'
+    });
 }]);
 
 
-app.controller('mainController', ['$scope', function($scope) {
+app.controller('mainCntrl', ['$scope', function($scope) {
+
+
+}]);
+
+app.controller('carouselCntrl', ['$scope', function($scope) {
+
+    $scope.myInterval = 0;
+    $scope.noWrapSlides = false;
+    $scope.active = 0;
+    var currIndex = 0;
+
+    $scope.slides = [
+        {
+        image: '../images/bedroom1-crop.jpg',
+        text: 'slide #1',
+        id: 0
+        },
+        {
+        image: '../images/livingroom-crop.jpg',
+        text: 'Awesome photograph',
+        id: 1
+        },
+        {
+        image: '../images/bedroom2-crop.jpg',
+        text: 'slide #3',
+        id: 2
+        }
+    ];
 
 }]);
